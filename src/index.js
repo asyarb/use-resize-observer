74a17a4f0d42f6bbb547d3455566203fdd4ab95b
git <asyarb@hawaii.edu>
2019-06-08 22:07:49 -1000

    feat: update deps, remove usecallback

--- src/index.js
+++ src/index.js
@@ -1,22 +1,20 @@
-import { useEffect, useState, useCallback } from 'react'
+import { useEffect, useState } from 'react'
 
 const IS_BROWSER = typeof window !== 'undefined'
 
 export const useResizeObserver = (ref, fn) => {
   const [sizes, setSizes] = useState({})
 
-  const handleResize = useCallback(
-    entries => {
-      const [entry] = entries.map(entry => entry)
-      if (fn) {
-        fn(entry)
-        return
-      }
+  const handleResize = entries => {
+    const [entry] = entries.map(entry => entry)
 
-      setSizes(entry.contentRect)
-    },
-    [fn]
-  )
+    if (fn) {
+      fn(entry)
+      return
+    }
+
+    setSizes(entry.contentRect)
+  }
 
   const [resizeObs] = useState(() =>
     IS_BROWSER ? new ResizeObserver(handleResize) : null

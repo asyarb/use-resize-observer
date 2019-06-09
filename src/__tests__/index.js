ed7ae1e51a62a10c4fda16407f5503420d7d7943
git <asyarb@hawaii.edu>
2019-06-08 22:15:05 -1000

    feat: include polyfill

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,6 +1,5 @@
 import { renderHook } from 'react-hooks-testing-library'
 import { useResizeObserver } from '../index'
-import { ResizeObserver } from '../__mocks__/ResizeObserver'
 
 describe('useResizeObserver', () => {
   test('returns an empty object if provided a falsey ref', () => {
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import ResizeObserver from 'resize-observer-polyfill'
 
 const IS_BROWSER = typeof window !== 'undefined'
 

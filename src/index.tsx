1510577866c5f98978493e2ecdc8d22301405207
git <asyarb@hawaii.edu>
2019-09-08 13:43:32 -1000

    fix: initial sizes

--- src/index.tsx
+++ src/index.tsx
@@ -47,7 +47,16 @@ export const useResizeObserver = ({
   element,
   callback,
 }: useResizeObserverProperties) => {
-  const [sizes, setSizes] = useState<DOMRectReadOnly>()
+  const [sizes, setSizes] = useState<DOMRectReadOnly>({
+    bottom: 0,
+    height: 0,
+    left: 0,
+    right: 0,
+    top: 0,
+    width: 0,
+    x: 0,
+    y: 0,
+  })
 
   const handleResize = (entries: ResizeObserverEntry[]) => {
     const [entry] = entries

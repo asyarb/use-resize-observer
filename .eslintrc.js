530e1f1d8a9f3989401f481c41832d0ad1847ad6
git <asyarb@hawaii.edu>
2019-09-08 12:53:57 -1000

    feat: init typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,3 @@
 module.exports = {
-  plugins: ['react', 'react-hooks'],
-  rules: {
-    'no-var': 'warn',
-    'no-unused-vars': 'warn',
-    'no-undef': 'error',
-
-    // react plugin - options
-    'react/jsx-uses-react': 'warn',
-    'react/jsx-uses-vars': 'warn',
-    'react/jsx-key': 'warn',
-    'react-hooks/rules-of-hooks': 'warn',
-    'react-hooks/exhaustive-deps': 'warn',
-  },
-  parser: 'babel-eslint',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 8, // optional, recommended 6+
-  },
+  extends: ['react-app'],
 }
---
+++ example/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+export const Example = () => {
+  return <div>Example</div>
+}
+
+ReactDOM.render(<Example />, document.getElementById('root'))
